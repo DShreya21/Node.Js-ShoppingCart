badab5c1f831404877571f471746d07c2289446b
Shreya Desai <17bmiit146@gmail.com>
2021-02-17 13:18:47 +0530

    Second commit

--- app.js
+++ app.js
@@ -10,11 +10,9 @@ var passport=require('passport');
 var flash=require('connect-flash');
 var validator=require('express-validator');
 var MongoStore=require('connect-mongo')(session);
-
 var indexRouter = require('./routes/index');
 var userRoutes = require('./routes/user');
 const { Store } = require('express-session');
-
 
 var app = express();
 mongoose.connect("mongodb://localhost:27017/shopping",{useNewUrlParser:true,useUnifiedTopology:true});
